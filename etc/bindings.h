23a23072e1b6c6393efb92e754fe796927579185
Greg Green <gpgreen@gmail.com>
2023-01-06 21:01:16 -0800

    Reran bindgen against freetype-2.12
    Added ftglyph.h to bindings.h
    Updated bindgen.sh to fix argument names
    changed freetype-sys to my fork

--- etc/bindings.h
+++ etc/bindings.h
@@ -1,6 +1,7 @@
-#include <freetype2/ft2build.h>
-#include <freetype2/freetype/freetype.h>
-#include <freetype2/freetype/ftlcdfil.h>
-#include <freetype2/freetype/tttables.h>
-#include <freetype2/freetype/ftmodapi.h>
-#include <freetype2/freetype/ftoutln.h>
+#include "freetype2/ft2build.h"
+#include "freetype2/freetype/freetype.h"
+#include "freetype2/freetype/ftlcdfil.h"
+#include "freetype2/freetype/tttables.h"
+#include "freetype2/freetype/ftmodapi.h"
+#include "freetype2/freetype/ftoutln.h"
+#include "freetype2/freetype/ftglyph.h"
